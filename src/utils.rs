44a48d4a1ce8c596d2c4460c61f770aafb225cb9
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-11-10 19:23:03 +0300

    tweak asserts

--- src/utils.rs
+++ src/utils.rs
@@ -310,7 +310,7 @@ pub fn next_u64_via_fill<R: RngCore + ?Sized>(rng: &mut R) -> u64 {
 #[inline]
 #[track_caller]
 pub fn read_u32_into(src: &[u8], dst: &mut [u32]) {
-    assert!(size_of_val(src) == size_of_val(dst));
+    assert_eq!(size_of_val(src), size_of_val(dst));
     for (out, chunk) in dst.iter_mut().zip(src.chunks_exact(4)) {
         *out = u32::from_le_bytes(chunk.try_into().unwrap());
     }
@@ -324,7 +324,7 @@ pub fn read_u32_into(src: &[u8], dst: &mut [u32]) {
 #[inline]
 #[track_caller]
 pub fn read_u64_into(src: &[u8], dst: &mut [u64]) {
-    assert!(size_of_val(src) == size_of_val(dst));
+    assert_eq!(size_of_val(src), size_of_val(dst));
     for (out, chunk) in dst.iter_mut().zip(src.chunks_exact(8)) {
         *out = u64::from_le_bytes(chunk.try_into().unwrap());
     }
